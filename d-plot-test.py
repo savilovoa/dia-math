14d1f193c39d5a21e6fa520ae172f3e3db869aa3
savilovoa <savilovoa@gmail.com>
2019-04-30 20:52:16 +0300

    ...

--- d-plot-test.py
+++ d-plot-test.py
@@ -3,18 +3,14 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from diafunc import linearfunc
 
+aspart = linearfunc('curve/aspart-in-data.txt')
 xs = np.arange(0, 120, 1)
 #xs = [0, 15, 30, 55, 75] 
 r = []
-r2 = []
-
-a =-3.06282216833E-002
-b =2.23132472411E-001
-c =-4.34107514124E-003
-d =2.07206130565E-005
-for i in xs:
-    r.append(a + b*i + c*i*i + d*i*i*i)
+for x in xs:
+    r.append(aspart(x))
     
 
 fig, ax = plt.subplots(figsize=(5, 3))
